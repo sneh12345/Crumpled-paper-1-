d44cba97646ef5e7d51a8a99b5d069934e142c36
sneh12345 <70284213+sneh12345@users.noreply.github.com>
2020-11-29 20:05:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+
+function setup() {
+	createCanvas(800, 700);
+    engine = Engine.create();
+	world = engine.world;
+	boxlside = new Box(700,550,150,15);
+	boxrside = new Box(770,500,15,100);
+	boxdside = new Box(630,500,15,100);
+	
+  ground = new Ground(500,560,10000,10);
+
+  paperObject = new Paper(100,530,24,10);
+
+	
+	
+	
+
+	
+	
+	Engine.run(engine);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  boxlside.display();
+  boxrside.display();
+  boxdside.display();
+  ground.display();
+  paperObject.display();
+ 
+
+ 
+ 
+
+  
+
+
+  drawSprites();
+ 
+
+
+}
+
+
+function keyPressed (){
+if(keyCode === 32){
+
+Matter.Body.applyForce(paperObject.body, paperObject.body.position,{x:23,y:-23});
+
+}
+
+
+
+
+}
+
